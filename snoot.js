e20a6b4ffdba15cc1509a98a24e7dfdeceaba810
Unknown <DTruong054@west-mec.org>
2018-08-06 15:28:31 -0700

    Added Javascript

--- snoot.js
+++ snoot.js
@@ -4,6 +4,19 @@
 
     Author: Daniel Truong
     Date: 8.6.18
-
-
 */
+
+"use strict";
+
+//Function to remove select list defaults
+function removeSelectDefaults() {
+    var emptyBoxes = document.getElementsByTagName("select");
+    alert("select lists: " + emptyBoxes.length);
+}
+
+//Page load event handlers
+if (window.addEventListener) {
+    window.addEventListener("load", removeSelectDefaults, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", removeSelectDefaults)
+}
