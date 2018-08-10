44dba2c9096f674db3eb24807eb85006fec44180
Unknown <DTruong054@west-mec.org>
2018-08-10 15:25:08 -0700

    Made some custom error messages

--- snoot.js
+++ snoot.js
@@ -10,6 +10,7 @@
 var twentyNine = document.createDocumentFragment();
 var thirty = document.createDocumentFragment();
 var thirtyOne = document.createDocumentFragment();
+var formValidity = true;
 
 //Function to remove select list defaults
 function removeSelectDefaults() {
@@ -93,6 +94,48 @@ function copyBillingAddress() {
     }
 }
 
+//Function to validate address - billing and delivery
+function validateAddress(fieldsetId) {
+    var inputElements = document.querySelectorAll("#" + fieldsetId + " input");
+    var errorDiv = document.querySelectorAll("#" + fieldsetId + " .errorMessage")[0];
+    var fieldsetValidity = true;
+    var elementCount = inputElements.length;
+    var currentElement;
+    try {
+        alert("I am executing the try clause");
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
+}
+
+//Function to validate entire form
+function validateForm(evt) {
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
+    }
+    formValidity = true;
+
+    validateAddress("billingAddress");
+    validateAddress("deliveryAddress");
+
+    if (formValidity === true) {
+        //Form is valid
+        //Prevent form default behavior
+        document.getElementById("errorText").innerHTML = "";
+        document.getElementById("errorText").style.display = "none";
+        document.getElementsByTagName("form")[0].submit();
+    } else {
+        document.getElementById("errorText").innerHTML = "Please fix the indicated problems and then resubmit your order";
+        document.getElementById("errorText").style.display = "block";
+        scroll(0,0);
+    }
+}
+
+
 
 //Function that sets up page on a load event
 function setUpPage() {
@@ -130,6 +173,13 @@ function createEventListener() {
     } else if (same.attachEvent) {
         same.attachEvent("onchange", copyBillingAddress);
     }
+
+    var form = document.getElementsByTagName("form")[0];
+    if (form.addEventListener) {
+        form.addEventListener("submit", validateForm, false);
+    } else if (form.attachEvent) {
+        form.attachEvent("onsubmit", validateForm);
+    }
 }
 
 
@@ -139,3 +189,5 @@ if (window.addEventListener) {
 } else if (window.attachEvent) {
     window.attachEvent("onload", setUpPage);
 }
+
+//Once upon a time there was a person who loved to slay monsters. He would always love to go out and kill monsters. That was something he really loves. He would always ignore his parent when they say he could not go and slay monsters. He was called Alain and with his trusty partner called Enclair he would kill every monster that got in his way. He would never have lost to a demon or a monster. Then one day a demon lord would have shown up in his village. The demon lord had a great power and would grant the hero any wish. The hero Alain has a choice to either keep adventuring with the girl he loves Enclair or go with the demon lord and have him grant his wish. Alain would regret whatever he chooses next. Even if he went with is love he won't get his wish, but if he goes with the demonlord he would lose his love Enclair. This was a really hard decsion for the hero Alain. After a night of fully resting he would choose Enclair and to slay the demon lord. He was not afraid of the demonlord. he asked for the help of his whole entire village. The villages would team up together with the hero Alain and try to take down teh demonlord. This however was a terrible idea since that demon lord was one of the 72 demon pillers of solomon. His name was Alcuder. He was a demon of simpathy. He defeated all the villagers and gave Alain a choice, either save Enclair or the rest of the village. He said "Give me a moment to talk with the elders and Enclair." Enclair wants Alain to save the village but she doesn't know that she is very important person not to just the hero but she is important to the whole village as well. She doesn't know her origens but Alain knows hers. He needs to tell the elders about Enclair and what he should choose.After a hour the hero came back to the demon lord and announced he wanted to save Enclair. The demon lord was shocked at his decision. He though he would surely save the Village, but insted he saved "that girl". Alcuder was baffled but held his end of the bargin. He would have slaughtered the village, and only Alain and Enclair was left. Enclair didn't know why Alain choose her and she was enraged. Her "family" has died and everyone of her friends has died. Alain would then suggest that they would go and hunt down that demon lord for revenge. Enclair however wanted to leave all this and just wanted to crawl into a corner and die. Alain would then go to the nearby village and buy her favorite foods. This was something that Enclair thought was sweet, but still hated Alain for what he did to the village. She wanted he life to be useful for once, but she only caused more angusih. She wanted to create a world where she and her love Robert has lived. However Robert has died in the village. She doesn't know that Alain loves her though. She only thinks of him as a freind. After they both had ate, they went on an adventure together. They would start their adventure in a place called Albion. In Albion they would be stopped by the guard caption Shannalottee. She would ask why are they in Albion, and what are their plans. Enclair replied "We are here because we started our adventure and we need some supplies." Alain said "We heard Albion has some great equipment for some starters like us". Shannalottee let them into the city. They decided to split up to gain what they had needed. Albion went to the blacksmith while Enclair went to the alchemy store. Alain would buy some leather armor and a short sword. Enclair would buy some potions and buy some daggers. She would also learn some healing spells. Once they where both done with their shopping they would meet up. They would then go to the guild hall and claim their money that the Elders left them. Enclair was still enraged t the demon lord for killing her beloved and her "family" Alain asks Enclair "What was the problem? Are you still sad about what I had done to the village?". Enclair just shok her head. She just looked at how calm Alain is and was wondering why he felt no saddness about what had just happened. Both of them where about to leave the city untill they got stopped once again by the guard captian Shannalottee. Shannalottee said "Where do you theves think your going?". Alain and Enclair was confused on why they were stopped. 
